dfc0545cd4211a1c63a1a5ce60e73a9393dc7298
Peter M Adrianov <peter.adrianov@gmail.com>
2025-12-15 20:54:46 +0300

    config: add max-lines warning to eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -24,6 +24,7 @@ export default [
             "complexity": ["warn", 10],
             "max-depth": ["warn", 4],
             "max-params": ["warn", 5],
+            "max-lines": ["warn", 400],
         }
     }
 ];
