1949c9c52937b63967b29da060ef5e75afc7e065
Peter M Adrianov <peter.adrianov@gmail.com>
2025-12-19 11:07:05 +0300

    refactor: simplify wall kick system and remove unused methods

--- assets/js/wallKicks.js
+++ assets/js/wallKicks.js
@@ -3,8 +3,5 @@ export class WallKickSystem {
     // This class is kept for compatibility but the complex logic has been moved to controls.js
     // The new system only uses basic, safe kicks that don't risk teleportation through walls
     
-    static getPrioritizedKickTests(pieceType, currentShape, board, piece, rotatedShape) {
-        // Return empty array - not used in the new simplified system
-        return [];
-    }
+
 }
